ba560a04965362f36233b39ea77c175d26b541a4
Ilyas Yamilev <14ilyas@mail.ru>
2019-11-16 22:43:46 +0500

    Added empty classes

---
+++ tasks/02/Permutation.java
@@ -0,0 +1,13 @@
+/* *****************************************************************************
+ *  Name: Yamilev Ilyas
+ *  Date: 16.11.2019
+ *  Description: Client program that takes an integer k as a command-line argument; reads a sequence
+ *  of strings from standard input using StdIn.readString(); and prints exactly k of them, uniformly
+ *  at random. Print each item from the sequence at most once.
+ **************************************************************************** */
+
+public class Permutation {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ tasks/02/RandomizedQueue.java
@@ -0,0 +1,52 @@
+/* *****************************************************************************
+ *  Name: Yamilev Ilyas
+ *  Date: 16.11.2019
+ *  Description: randomized queue is similar to a stack or queue, except that the item removed is
+ *  chosen uniformly at random among items in the data structure.
+ **************************************************************************** */
+
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    // construct an empty randomized queue
+    public RandomizedQueue(){
+        
+    }
+
+    // is the randomized queue empty?
+    public boolean isEmpty(){
+        
+    }
+
+    // return the number of items on the randomized queue
+    public int size(){
+        
+    }
+
+    // add the item
+    public void enqueue(Item item){
+        
+    }
+
+    // remove and return a random item
+    public Item dequeue(){
+        
+    }
+
+    // return a random item (but do not remove it)
+    public Item sample(){
+        
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator(){
+        
+    }
+
+    // unit testing (required)
+    public static void main(String[] args){
+        
+    }
+
+}
