a42f6af4837751dbbb8ad47692f92b51a83bc917
Ilyas Yamilev <14ilyas@mail.ru>
2019-11-17 22:25:45 +0500

    Filled deque fields and methods

--- tasks/02/Deque.java
+++ tasks/02/Deque.java
@@ -9,48 +9,137 @@
 import java.util.Iterator;
 
 public class Deque<Item> implements Iterable<Item> {
-
+    
+    private int size = 0;
+    private Node first = null;
+    private Node last = null;
+    private class Node {
+        Item value;
+        Node next;
+        Node prev;
+    }
     // construct an empty deque
-    public Deque(){
+    public Deque() {
         
     }
 
     // is the deque empty?
-    public boolean isEmpty(){
-        
+    public boolean isEmpty() {
+        return size == 0;
     }
 
     // return the number of items on the deque
-    public int size(){
-        
+    public int size() {
+        return size;
     }
 
     // add the item to the front
-    public void addFirst(Item item){
-        
+    public void addFirst(Item item) {
+        if (item == null)
+            throw new IllegalArgumentException("Tried to add null object");
+        Node newfirst = new Node();
+        newfirst.value = item;
+        if (size == 0) {
+            first = newfirst;
+            last = newfirst;
+            first.next = null;
+            first.prev = null;
+            size = 1;
+            return;
+        }
+        newfirst.next = first;
+        newfirst.prev = null;
+        first.prev = newfirst;
+        first = newfirst;
+        ++size;
     }
 
     // add the item to the back
-    public void addLast(Item item){
-        
+    public void addLast(Item item) {
+        if (item == null)
+            throw new IllegalArgumentException("Tried to add null object");
+        Node newlast = new Node();
+        newlast.value = item;
+        if (size == 0) {
+            first = newlast;
+            last = newlast;
+            first.next = null;
+            first.prev = null;
+            size = 1;
+            return;
+        }
+        last.next = newlast;
+        newlast.prev = last;
+        newlast.next = null;
+        last = newlast;
+        ++size;
     }
 
     // remove and return the item from the front
-    public Item removeFirst(){
-        
+    public Item removeFirst() {
+        if (size == 0) {
+            throw new java.util.NoSuchElementException("Error: trying remove element from empty deque");
+        }
+        Node oldfirst = first;
+        Item val = oldfirst.value;
+        if (size == 1) {
+            first = null;
+            last = null;
+            size = 0;
+            return val;
+        }
+        first = first.next;
+        first.prev = null;
+        oldfirst.next = null;
+        --size;
+        return val;
     }
 
     // remove and return the item from the back
-    public Item removeLast(){
-        
+    public Item removeLast() {
+        if (size == 0) {
+            throw new java.util.NoSuchElementException("Error: trying remove element from empty deque");
+        }
+        Node oldlast = last;
+        Item val = oldlast.value;
+        if (size == 1) {
+            first = null;
+            last = null;
+            size = 0;
+            return val;
+        }
+        last = last.prev;
+        last.next = null;
+        oldlast.prev = null;
+        --size;
+        return val;
     }
 
     // return an iterator over items in order from front to back
-    public Iterator<Item> iterator(){
-        
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+        public boolean hasNext() {
+            return current != null;
+        }
+        public Item next() {
+            if (current == null)
+                throw new java.util.NoSuchElementException("No more elemnts in deque!");
+            Item item = current.value;
+            current   = current.next;
+            return item;
+        }
+        public void remove() {
+            throw new UnsupportedOperationException("remove() is not supproted in this implementation");
+        }
     }
 
     // unit testing (required)
-    public static void main(String[] args);
+    public static void main(String[] args) {
+        
+    }
 
 }
--- tasks/02/RandomizedQueue.java
+++ tasks/02/RandomizedQueue.java
@@ -5,47 +5,45 @@
  *  chosen uniformly at random among items in the data structure.
  **************************************************************************** */
 
-import java.util.Iterator;
-
-public class RandomizedQueue<Item> implements Iterable<Item> {
+public class RandomizedQueue<Item> /*implements Iterable<Item> */ {
 
     // construct an empty randomized queue
-    public RandomizedQueue(){
+    public RandomizedQueue() {
         
     }
 
     // is the randomized queue empty?
-    public boolean isEmpty(){
-        
+    public boolean isEmpty() {
+        return true;
     }
 
     // return the number of items on the randomized queue
-    public int size(){
-        
+    public int size() {
+        return 0;
     }
 
     // add the item
-    public void enqueue(Item item){
+    public void enqueue(Item item) {
         
     }
 
     // remove and return a random item
-    public Item dequeue(){
-        
+    public Item dequeue() {
+        return null;
     }
 
     // return a random item (but do not remove it)
-    public Item sample(){
-        
+    public Item sample() {
+        return null;
     }
 
     // return an independent iterator over items in random order
-    public Iterator<Item> iterator(){
-        
-    }
+    // public Iterator<Item> iterator() {
+    //     return;
+    // }
 
     // unit testing (required)
-    public static void main(String[] args){
+    public static void main(String[] args) {
         
     }
 
