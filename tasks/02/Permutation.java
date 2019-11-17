cfb0c3f10bbd3c2c0ee817e5b86a598570ab782c
Ilyas Yamilev <14ilyas@mail.ru>
2019-11-17 23:55:57 +0500

    Added main() for Permutation

--- tasks/02/Permutation.java
+++ tasks/02/Permutation.java
@@ -6,8 +6,22 @@
  *  at random. Print each item from the sequence at most once.
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdIn;
+
 public class Permutation {
     public static void main(String[] args) {
-
+        int k = Integer.parseInt(args[0]);
+        String str = "";
+        RandomizedQueue<String> testQueue = new RandomizedQueue<String>();
+        try {
+            while (true) {
+                str = StdIn.readString();
+                testQueue.enqueue(str);
+            }
+        }
+        catch (java.util.NoSuchElementException e) {};
+        
+        for (int i = 0; i < k; i++)
+            System.out.printf("%s\n", testQueue.dequeue());
     }
 }
